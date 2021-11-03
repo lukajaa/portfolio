052e7bf660af2641ccfd790e220a076641c912f9
LUCAS-PC\Lucas <lucaskchang@gmail.com>
2021-11-02 20:35:49 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero is-info is-fullheight header-image text-shadow">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title big-title">Lucas Chang</h1>
+          <p class="subtitle is-2">Student, Programmer, Runner</p>
+
+          <a href="https://github.com/FairfieldBW" target="_blank"><b-icon class="mx-1" icon="github" size="is-large" href="https://diogotc.com/"></b-icon></a>
+          <a href="https://twitter.com/FairfieldBW" target="_blank"><b-icon class="mx-1" icon="twitter" size="is-large"></b-icon></a>
+          <a href="https://www.youtube.com/channel/UCHXhJzu4eN8CjcPpppsSk_g" target="_blank"><b-icon class="mx-1" icon="youtube" size="is-large"></b-icon></a>
+          <a href="https://www.instagram.com/chucas.lang/" target="_blank"><b-icon class="mx-1" icon="instagram" size="is-large"></b-icon></a>
+          <a @click="alert()"><b-icon class="mx-1" icon="discord" size="is-large"></b-icon></a>
+          <a href="https://www.athletic.net/profile/LucasChang1" target="_blank"><b-icon class="mx-1" icon="shoe-sneaker" size="is-large"></b-icon></a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    methods: {
+      alert() {
+        this.$buefy.dialog.alert({
+            message: `Fairfield#4104`,
+            type: 'is-info',
+        })
+      }
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<!-- <b-button class="mr-4" size="is-medium" type="is-info" rounded outlined inverted>Projects</b-button> -->
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '../src/assets/styles.scss';
+import Buefy from 'buefy'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 new Vue({
   render: h => h(App),
